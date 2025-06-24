4a09d6cba3a99b4d32c1ecc86b42e786cadfb159
Pann <136094314+scripterpan@users.noreply.github.com>
2025-06-24 11:28:34 +0700

    Create BTools.lua

---
+++ Tools/BTools.lua
@@ -0,0 +1,26 @@
+--[[
+A distribution of https://wearedevs.net/scripts
+Last updated August 2, 2021
+Made by wearedevs
+
+Description: Gives your player the old system of building tools
+
+Instruction: Inject this script into any game using a Lua executor. 
+]]
+
+backpack = game:GetService("Players").LocalPlayer.Backpack
+
+hammer = Instance.new("HopperBin")
+hammer.Name = "Hammer"
+hammer.BinType = 4
+hammer.Parent = backpack
+
+cloneTool = Instance.new("HopperBin")
+cloneTool.Name = "Clone"
+cloneTool.BinType = 3
+cloneTool.Parent = backpack
+
+grabTool = Instance.new("HopperBin")
+grabTool.Name = "Grab"
+grabTool.BinType = 2
+grabTool.Parent = backpack
