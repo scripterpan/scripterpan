369a7cb75ee5f687d11541bfb47f18ec0d1b7bb7
Pann <136094314+scripterpan@users.noreply.github.com>
2025-07-10 17:16:01 +0700

    Update Example.lua

--- ESP-Template/Example.lua
+++ ESP-Template/Example.lua
@@ -1,13 +1,8 @@
---ESP Module for Roblox
-
---This is a simple ESP (highlight) script you can load into Roblox scripts to highlight any model with a glowing outline.
-
--- How to use
-
+-- Example
 
 local espLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ESP-Template/Script.lua"))()
 
-local target = workspace:WaitForChild("Beta")  -- Change to the model you want to highlight
+local target = workspace:WaitForChild("Example")  -- Change to the model you want to highlight
 local color = Color3.fromRGB(255, 0, 0)        -- Change to your preferred color
 
 espLib(target, color)  -- Activates ESP on the target
