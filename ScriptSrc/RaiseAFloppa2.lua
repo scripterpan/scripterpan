48ad3abf0e45070a7654487a8c7c90974ec99d77
Pann <136094314+scripterpan@users.noreply.github.com>
2025-07-12 11:59:35 +0700

    Update RaiseAFloppa2.lua

--- ScriptSrc/RaiseAFloppa2.lua
+++ ScriptSrc/RaiseAFloppa2.lua
@@ -1,1 +1,82 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
+local Window = WindUI:CreateWindow({
+    Title = "Raise A Floppa 2",
+    Icon = "door-open",
+    Author = "Pann Hub",
+    Folder = "PannHub-WindUi-RAF2",
+    Size = UDim2.fromOffset(500, 360),
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    Background = "", -- rbxassetid only
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+})
+
+Window:EditOpenButton({
+    Title = "Pann Hub (Raise A Floppa 2)",
+    Icon = "book-open",
+    CornerRadius = UDim.new(0,16),
+    StrokeThickness = 2,
+    Color = ColorSequence.new( -- gradient
+        Color3.fromHex("FF0F7B"), 
+        Color3.fromHex("F89B29")
+    ),
+    OnlyMobile = false,
+    Enabled = true,
+    Draggable = true,
+
+})   
+
+
+local info = Window:Tab({
+    Title = "Information",
+    Icon = "scroll-text",
+    Locked = false,
+})
+
+local main = Window:Tab({
+    Title = "Main",
+    Icon = "users",
+    Locked = false,
+})
+
+local cook = Window:Tab({
+    Title = "Cook",
+    Icon = "crosshair",
+    Locked = false,
+})
+
+local auto = Window:Tab({
+    Title = "Automatic",
+    Icon = "eye",
+    Locked = false,
+})
+
+
+local tp = Window:Tab({
+    Title = "Teleport",
+    Icon = "badge-plus",
+    Locked = false,
+})
+
+local misc = Window:Tab({
+    Title = "Miscellaneous",
+    Icon = "badge-plus",
+    Locked = false,
+})
+
+local mul = Window:Tab({
+    Title = "Multiplayer",
+    Icon = "badge-plus",
+    Locked = false,
+})
+
+local Other = Window:Tab({
+    Title = "Other",
+    Icon = "badge-plus",
+    Locked = false,
+})
