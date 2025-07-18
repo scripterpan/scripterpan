904e27d95952d3e0cb365eedbd3ab6a2712fb3d2
Pann <136094314+scripterpan@users.noreply.github.com>
2025-07-18 12:26:54 +0700

    Update RaiseAFloppa2.lua

--- ScriptSrc/RaiseAFloppa2.lua
+++ ScriptSrc/RaiseAFloppa2.lua
@@ -55,14 +55,122 @@ do
     })
 
     
-    local main = Tabs.main:Tab({ Title = "Main", Icon = "album" })
-    local cook = Tabs.main:Tab({ Title = "Cook", Icon = "cooking-pot" })
-    local auto = Tabs.main:Tab({ Title = "Automatic", Icon = "code" })
-    local Tp = Tabs.main:Tab({ Title = "Teleport", Icon = "arrow-down-to-dot" })
-    local gemisc = Tabs.misc:Tab({ Title = "General Misc", Icon = "tv-minimal"})
-    local plmisc = Tabs.misc:Tab({ Title = "Player Misc", Icon = "person-standing"})
-    local mul = Tabs.Other:Tab({ Title = "Multiplayer", Icon = "users-round" })
-    local src = Tabs.Other:Tab({ Title = "Universal Script & Tools", Icon = "scroll-text" })
-    local exec = Tabs.Other:Tab({ Title = "Built-in Executor (Ok wth)", Icon = "toggle-left" })
+    Tabs.main1 = Tabs.main:Tab({ Title = "Main", Icon = "album" })
+    Tabs.cook = Tabs.main:Tab({ Title = "Cook", Icon = "cooking-pot" })
+    Tabs.auto = Tabs.main:Tab({ Title = "Automatic", Icon = "code" })
+    Tabs.Tp = Tabs.main:Tab({ Title = "Teleport", Icon = "arrow-down-to-dot" })
+    Tabs.gemisc = Tabs.misc:Tab({ Title = "General Misc", Icon = "tv-minimal"})
+    Tabs.plmisc = Tabs.misc:Tab({ Title = "Player Misc", Icon = "person-standing"})
+    Tabs.mul = Tabs.Other:Tab({ Title = "Multiplayer", Icon = "users-round" })
+    Tabs.src = Tabs.Other:Tab({ Title = "Universal Script & Tools", Icon = "scroll-text" })
+    Tabs.exec = Tabs.Other:Tab({ Title = "Built-in Executor (Ok wth)", Icon = "toggle-left" })
 
 end
+
+
+-- setupain function for teleport 
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+
+
+-- setup tp back
+local originalCFrame = humanoidRootPart.CFrame
+
+        local function teleportBack()
+            humanoidRootPart.CFrame = originalCFrame
+        end
+
+
+-- Setup Tp tp Floppa
+
+        local function teleportToFloppa()
+            local floppa = workspace:FindFirstChild("Floppa")
+
+            if floppa then
+                humanoidRootPart.CFrame = floppa.HumanoidRootPart.CFrame
+            end
+        end
+
+
+
+
+
+-- setup interact 
+
+function interact()
+    local function getVisiblePrompt()
+        local hrp = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+        if not hrp then return end
+
+        for _, v in ipairs(workspace:GetDescendants()) do
+            if v:IsA("ProximityPrompt") and v.Enabled and v.Parent:IsA("BasePart") then
+                local dist = (hrp.Position - v.Parent.Position).Magnitude
+                if dist <= v.MaxActivationDistance then
+                    return v
+                end
+            end
+        end
+    end
+
+    local prompt = getVisiblePrompt()
+    if prompt then
+        fireproximityprompt(prompt)
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+Tabs.main1:Button({
+    Title = "Floppa's Moodlets",
+    Desc = "Feed Floppa Hot Chocolate, Lemonade When Floppa Is Cold, Hot",
+    Callback = function() 
+        humanoidRootPart.CFrame = CFrame.new(-5.52987146, 69.0936737, -108.875877, 0.307733119, 1.24310662e-09, 0.9514727, -2.64562194e-09, 1, -4.50839088e-10, -0.9514727, -2.37849895e-09, 0.307733119)
+
+        task.wait(0.4)
+
+        interact()
+
+        task.wait(0.3)
+
+        teleportToFloppa()
+
+        task.wait(0.4)
+
+        interact()
+
+        task.wait(0.45)
+
+        teleportBack()
+end
+})
+
+
+Tabs.main1:Button({
+    Title = "Activate Meteorite Magnet",
+    Desc = "Activate Meteorite Magnet if purchase",
+    Callback = function() 
+          local function hasMeteoriteMagnet()
+            local meteoriteMagnet = workspace.Unlocks:FindFirstChild("Meteor Magnet")  -- Check if Meteor Magnet is in the Unlocks folder
+            return meteoriteMagnet ~= nil
+        end
+
+        if hasMeteoriteMagnet() then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-46.8612709, 153.73613, -41.5506172)
+            task.wait(0.45)
+            interact () 
+            task.wait(0.5)
+            teleportBack()
+end
+end
+})
