f8c239ea780746b66e0d631f77a5449105a1b36c
Pann <136094314+scripterpan@users.noreply.github.com>
2025-07-22 00:30:10 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,16 +1,15 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
-
 local creator = game.CreatorId
 
 local games = {
     [14179659] = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/RaiseAFloppa2.lua', -- Raise A Floppa 2
-    [8509669] = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/SolsRNG.lua', -- Sols RNG
-    [9482918] = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/TheMimic.lua', -- The Mimic
-    [8401765] = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/JobApplication.lua', -- Survive The Job Application
-    [32900163] = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/PETAPETA.lua', -- PETAPETA (Original and School)
-    [33541525] = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/7DTL.lua', -- 7 Days To Live
-    [7314991] = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/Nightlight.lua', -- Nightlight (Horror)
+    [8509669]  = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/SolsRNG.lua',        -- Sols RNG
+    [9482918]  = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/TheMimic.lua',       -- The Mimic
+    [8401765]  = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/JobApplication.lua', -- Survive The Job Application
+    [32900163] = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/PETAPETA.lua',       -- PETAPETA (Original and School)
+    [33541525] = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/7DTL.lua',           -- 7 Days To Live
+    [7314991]  = 'https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/Nightlight.lua',     -- Nightlight (Horror)
 }
 
 WindUI:Popup({
@@ -38,7 +37,9 @@ WindUI:Popup({
                     Icon = "info",
                     Duration = 5,
                 })
+
                 task.wait(2.5)
+
                 if games[creator] then
                     WindUI:Notify({
                         Title = "Supported!",
@@ -46,13 +47,70 @@ WindUI:Popup({
                         Icon = "info",
                         Duration = 5,
                     })
+
+                    task.wait(1.5)
                     loadstring(game:HttpGet(games[creator]))()
+
+                    WindUI:Notify({
+                        Title = "Done!",
+                        Content = "Loading finished",
+                        Icon = "info",
+                        Duration = 5,
+                    })
                 else
                     WindUI:Notify({
                         Title = "Unsupported",
                         Content = "We'll be adding the list of supported games soon",
                         Icon = "info",
                         Duration = 5,
+                    })
+
+                    task.wait(2.5)
+
+                    WindUI:Notify({
+                        Title = "Do you want to load our universal script?",
+                        Content = "You can use this in almost any Roblox game",
+                        Icon = "info",
+                        Duration = 5,
+                    })
+
+                    task.wait(2.5)
+
+                    WindUI:Popup({
+                        Title = "Do you want to load our universal script?",
+                        Icon = "info",
+                        Content = "You can use this in almost any Roblox game",
+                        Buttons = {
+                            {
+                                Title = "No, Thanks",
+                                Callback = function() end,
+                                Variant = "Tertiary",
+                            },
+                            {
+                                Title = "Sure",
+                                Icon = "arrow-right",
+                                Callback = function()
+                                    WindUI:Notify({
+                                        Title = "Loading",
+                                        Content = "Please wait",
+                                        Icon = "info",
+                                        Duration = 5,
+                                    })
+
+                                    task.wait(2)
+
+                                    loadstring(game:HttpGet("https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ScriptSrc/Universal.lua"))()
+
+                                    WindUI:Notify({
+                                        Title = "Done!",
+                                        Content = "Loading finished",
+                                        Icon = "info",
+                                        Duration = 5,
+                                    })
+                                end,
+                                Variant = "Primary",
+                            }
+                        }
                     })
                 end
             end,
