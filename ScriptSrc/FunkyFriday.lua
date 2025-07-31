c64676db3f8d896cdcfbbba903ac6d06fd9e5270
Pann <136094314+scripterpan@users.noreply.github.com>
2025-07-31 12:31:55 +0700

    Create FunkyFriday.lua

---
+++ ScriptSrc/FunkyFriday.lua
@@ -0,0 +1,69 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "Raise A Floppa 2",
+    Icon = "cat",
+    Author = "Pann Hub",
+    Folder = "PannHub-WindUi-RAF2",
+    Size = UDim2.fromOffset(520, 360),
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    Background = "", -- rbxassetid only
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+    User = {
+        Enabled = true,
+        Anonymous = false,
+    }
+})
+
+Window:EditOpenButton({
+    Title = "Pann Hub (Raise A Floppa 2)",
+    Icon = "cat",
+    CornerRadius = UDim.new(0,16),
+    StrokeThickness = 2,
+    Color = ColorSequence.new( -- gradient
+        Color3.fromHex("#32a89d"), 
+        Color3.fromHex("32a0a8")
+    ),
+    OnlyMobile = false,
+    Enabled = true,
+    Draggable = true,
+
+})   
+
+local Tabs = {}
+
+do
+
+Tabs.stat = Window:Section({
+        Title = "Status",
+        Opened = true,
+    })
+    Tabs.main = Window:Section({
+        Title = "Main",
+        Opened = true,
+    })
+
+    Tabs.misc = Window:Section({
+        Title = "Miscellaneous",
+        Opened = true,
+    })
+    
+    
+    Tabs.Other = Window:Section({
+        Title = "Other",
+        Opened = true,
+    })
+
+    
+    Tabs.StatPlayer = Tabs.stat:Tab({ Title = "Player's Status", Icon = "speech" })
+    Tabs.StatServer = Tabs.stat:Tab({ Title = "Server's Status", Icon = "server" })
+    Tabs.main1 = Tabs.main:Tab({ Title = "Main", Icon = "album" })
+    Tabs.gemisc = Tabs.misc:Tab({ Title = "Misc", Icon = "tv-minimal"})
+    Tabs.src = Tabs.Other:Tab({ Title = "Universal Script & Tools", Icon = "scroll-text" })
+    
+end
